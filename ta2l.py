84b35ae19d0f94f60f162b6c5e73bbbeb0a3829a
Alex Gunter <akg7634@gmail.com>
2016-04-02 13:11:30 -0500

    Restructured to use functions

--- ta2l.py
+++ ta2l.py
@@ -1,92 +1,117 @@
 from __future__ import division
 
+import os
+import pickle
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xgboost as xgb
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import GenericUnivariateSelect
+from sklearn.feature_selection import chi2
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.pipeline import Pipeline
 from sklearn.svm import OneClassSVM
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
-from subprocess import check_output
-print(check_output(["ls", "input/"]).decode("utf8"))
+def main():
+    X_train, y_train, X_test, id_test = init()
+    X_train, X_test = preprocess(X_train, y_train, X_test)
 
-# Any results you write to the current directory are saved as output.
+    # classifier
+    clf = xgb.XGBClassifier(missing=np.nan, max_depth=5, n_estimators=350,\
+        learning_rate=0.03, nthread=4, subsample=0.95, colsample_bytree=0.85,\
+        seed=4242)
+
+    X_fit, X_eval, y_fit, y_eval= train_test_split(X_train, y_train,\
+        test_size=0.3)
+
+    # fitting
+    print("== Training Classifier ==")
+    clf.fit(X_train, y_train, early_stopping_rounds=20, eval_metric="auc",\
+        eval_set=[(X_eval, y_eval)])
+
+    auc = roc_auc_score(y_train, clf.predict_proba(X_train)[:,1])
+    print('Overall AUC:', auc)
+
+    # predicting
+    y_pred= clf.predict_proba(X_test)[:,1]
+
+    submission = pd.DataFrame({"ID":id_test, "TARGET":y_pred})
+    submission.to_csv("submission.csv", index=False)
+
+    print('Completed!')
 
 
-# load data
-df_train = pd.read_csv('input/train.csv')
-df_test = pd.read_csv('input/test.csv')
+def init():
+    archfile = 'archive.pckl'
+    if os.path.isfile(archfile):
+        print("== Reloading Data ==")
+        archive = pickle.load(open(archfile, 'rb'))
+    else:
+        print("== Loading Data ==")
+        archive = load_data()
+        pickle.dump(archive, open(archfile, 'wb'))
 
-# remove constant columns
-remove = []
-for col in df_train.columns:
-    if df_train[col].std() == 0:
-        remove.append(col)
+    return archive
 
-df_train.drop(remove, axis=1, inplace=True)
-df_test.drop(remove, axis=1, inplace=True)
+def load_data():
+    # load data
+    df_train = pd.read_csv('input/train.csv')
+    df_test = pd.read_csv('input/test.csv')
 
-# remove duplicated columns
-remove = []
-c = df_train.columns
-for i in range(len(c)-1):
-    v = df_train[c[i]].values
-    for j in range(i+1,len(c)):
-        if np.array_equal(v,df_train[c[j]].values):
-            remove.append(c[j])
+    # remove constant columns
+    remove = []
+    for col in df_train.columns:
+        if df_train[col].std() == 0:
+            remove.append(col)
 
-df_train.drop(remove, axis=1, inplace=True)
-df_test.drop(remove, axis=1, inplace=True)
+    df_train.drop(remove, axis=1, inplace=True)
+    df_test.drop(remove, axis=1, inplace=True)
 
-y_train = df_train['TARGET'].values
-X_train = df_train.drop(['ID','TARGET'], axis=1).values
+    # remove duplicated columns
+    remove = []
+    c = df_train.columns
+    for i in range(len(c)-1):
+        v = df_train[c[i]].values
+        for j in range(i+1,len(c)):
+            if np.array_equal(v,df_train[c[j]].values):
+                remove.append(c[j])
 
-id_test = df_test['ID']
-X_test = df_test.drop(['ID'], axis=1).values
+    df_train.drop(remove, axis=1, inplace=True)
+    df_test.drop(remove, axis=1, inplace=True)
 
-X_merge = np.concatenate((X_train, X_test), axis=0)
-sep = X_train.shape[0]
+    y_train = df_train['TARGET'].values
+    X_train = df_train.drop(['ID','TARGET'], axis=1).values
 
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.feature_selection import GenericUnivariateSelect
-from sklearn.feature_selection import chi2
-# Scale features to range [0, 1]
-scale = MinMaxScaler()
-X_merge = scale.fit_transform(X_merge)
+    id_test = df_test['ID']
+    X_test = df_test.drop(['ID'], axis=1).values
 
-X_train = X_merge[:sep]
-X_test = X_merge[sep:]
+    return X_train, y_train, X_test, id_test
 
-# Choose top features as ranked by chi squared test
-gus = GenericUnivariateSelect(score_func=chi2, mode="k_best", param=150)
-gus.fit(X_train, y_train)
-X_train = gus.transform(X_train)
-X_test = gus.transform(X_test)
+def preprocess(X_train, y_train, X_test):
+    print("== Preprocessing Data ==")
+    X_merge = np.concatenate((X_train, X_test), axis=0)
+    sep = X_train.shape[0]
 
-# length of dataset
-len_train = len(X_train)
-len_test  = len(X_test)
+    # Scale features to range [0, 1]
+    scale = MinMaxScaler()
+    X_merge = scale.fit_transform(X_merge)
 
-# classifier
-clf = xgb.XGBClassifier(missing=np.nan, max_depth=5, n_estimators=350, learning_rate=0.03, nthread=4, subsample=0.95, colsample_bytree=0.85, seed=4242)
+    X_train = X_merge[:sep]
+    X_test = X_merge[sep:]
 
-X_fit, X_eval, y_fit, y_eval= train_test_split(X_train, y_train, test_size=0.3)
+    # Choose top features as ranked by chi squared test
+    gus = GenericUnivariateSelect(score_func=chi2, mode="k_best", param=306)
+    gus.fit(X_train, y_train)
+    X_train = gus.transform(X_train)
+    X_test = gus.transform(X_test)
 
-# fitting
-clf.fit(X_train, y_train, early_stopping_rounds=20, eval_metric="auc", eval_set=[(X_eval, y_eval)])
+    return X_train, X_test
 
-print('Overall AUC:', roc_auc_score(y_train, clf.predict_proba(X_train)[:,1]))
 
-# predicting
-y_pred= clf.predict_proba(X_test)[:,1]
-
-submission = pd.DataFrame({"ID":id_test, "TARGET":y_pred})
-submission.to_csv("submission.csv", index=False)
-
-print('Completed!')
+if __name__ == '__main__':
+    main()
